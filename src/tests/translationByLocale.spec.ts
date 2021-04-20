2e0a6c04c30a04aa73e2bb795f65da978f258f1c
Edvinas Bazevicius <edvinasbazevicius@gmail.com>
2021-04-20 13:00:09 +0300

    Improve test case variables to make more sense:)

--- src/tests/translationByLocale.spec.ts
+++ src/tests/translationByLocale.spec.ts
@@ -2,18 +2,18 @@ import translationByLocale from '../translationByLocale';
 
 describe('translationByLocale', () => {
     test('returns correct translation string when provided locale exists in strings', () => {
-        expect(translationByLocale({en: 'Name', fr: 'name'}, 'en')).toStrictEqual('Name');
+        expect(translationByLocale({en: 'en', fr: 'fr'}, 'en')).toStrictEqual('en');
     });
     test('returns correct translation object when provided locale exists in strings', () => {
-        expect(translationByLocale({en: {name: 'Name'}, fr: {name: 'Name'}}, 'en')).toStrictEqual({name: 'Name'});
+        expect(translationByLocale({en: {lang: 'en'}, fr: {lang: 'fr'}}, 'en')).toStrictEqual({lang: 'en'});
     });
     test('fallbacks to correct translation object when provided locale doesnt exist in strings', () => {
-        expect(translationByLocale({en: {name: 'Name'}, fr: {name: 'Name'}}, 'en-ZA')).toStrictEqual({name: 'Name'});
+        expect(translationByLocale({en: {lang: 'en'}, fr: {lang: 'fr'}}, 'en-ZA')).toStrictEqual({lang: 'en'});
     });
     test('returns \'en\' translation object when provided locale doesnt exist in strings', () => {
-        expect(translationByLocale({en: {name: 'Name'}, fr: {name: 'Name'}}, 'de')).toStrictEqual({name: 'Name'});
+        expect(translationByLocale({en: {lang: 'en'}, fr: {lang: 'fr'}}, 'de')).toStrictEqual({lang: 'en'});
     });
     test('returns \'undefined\' when provided locale doesnt exist in strings and there is nothing to fall back to', () => {
-        expect(translationByLocale({ru: {name: 'Name'}, fr: {name: 'Name'}}, 'de')).toBe(undefined);
+        expect(translationByLocale({ru: {lang: 'ru'}, fr: {lang: 'fr'}}, 'de')).toBe(undefined);
     });
 });
